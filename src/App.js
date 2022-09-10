e4387bc044c5546a999a619bffcc32038d3c868c
befor921after <hendrix291lincoln@gmail.com>
2022-09-10 21:12:21 +0430

    first commit in my repository

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+import {
+  AddContact,
+  Contact,
+  Contacts,
+  EditContact,
+  Navbar,
+} from "./components";
+
+import { getAllContacts, getAllGroups } from "./services/contactService";
+
+import "./App.css";
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [getContacts, setContacts] = useState([]);
+  const [getGroups, setGroups] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const { data: contactsData } = await getAllContacts();
+        const { data: groupsData } = await getAllGroups();
+
+        setContacts(contactsData);
+        setGroups(groupsData);
+
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/contacts" />} />
+        <Route
+          path="/contacts"
+          element={<Contacts contacts={getContacts} loading={loading} />}
+        />
+        <Route path="/contacts/add" element={<AddContact />} />
+        <Route path="/contacts/:contactId" element={<Contact />} />
+        <Route path="/contacts/edit/:contactId" element={<EditContact />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
